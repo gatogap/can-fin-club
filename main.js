91d987b5f33944924a9e666079789947c5f47fcf
Emma <emma.a.mayoral@gmail.com>
2023-02-07 01:34:52 -0800

    FAQ

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 class MyHeader extends HTMLElement {
     connectedCallback()
     {
-    this.innerHTML = '<div class="headerWrapper"> <div class="box a"> <header> <h1 style = "font-size: 25px;">&nbsp;Finance<br>&nbsp;&<br>&nbsp;Investment&nbsp;<br></h1> </header> </div> <div class="box b"> <nav> <table width="100%"> <th><h4><a href="index.html">Home</a></h4></th> <th><h4><a href="Articles.html">Articles</a></h4></th> <th><h4><a href="Recordings.html">Recordings</a></h4></th> <th><h4><a href="FAQ.html">Questionnaire</a></h4></th> <th><h4><a href="Resources.html">Resources</a></h4></th> </table> </nav> </div> </div>'
+    this.innerHTML = '<div class="headerWrapper"> <div class="box a"> <header> <h1 style = "font-size: 25px;">&nbsp;Finance<br>&nbsp;&<br>&nbsp;Investment&nbsp;<br></h1> </header> </div> <div class="box b"> <nav> <table width="100%"> <th><h4><a href="index.html">Home</a></h4></th> <th><h4><a href="Articles.html">Articles</a></h4></th> <th><h4><a href="Recordings.html">Recordings</a></h4></th> <th><h4><a href="Questionnaire.html">FAQ</a></h4></th> <th><h4><a href="Resources.html">Resources</a></h4></th> </table> </nav> </div> </div>'
     }
 }
 
